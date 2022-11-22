9036e2fbe78758b0e57192d92f19fdd4956e18e8
wjryan5 <43756110+wjryan5@users.noreply.github.com>
2022-11-22 12:42:25 +0000

    8

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    // let name = 'name';
+
+    const [name, setName] = useState('name1');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('name2');
+        setAge(30)
+    } 
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }
