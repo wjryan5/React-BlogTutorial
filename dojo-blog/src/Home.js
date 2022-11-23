1aba42c617ca95f42267ef119bc0ed85b0b47ba0
wjryan5 <43756110+wjryan5@users.noreply.github.com>
2022-11-23 09:34:53 +0000

    15

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,21 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "New Website", body: "lorem ipsum...", author: "author1", id: 1 },
     { title: "Blog post 2!", body: "lorem ipsum...", author: "author2", id: 2 },
-    {
-      title: "Awful website",
-      body: "lorem ipsum...",
-      author: "author1",
-      id: 3,
-    },
+    { title: "Awful website", body: "lorem ipsum...", author: "author1", id: 3, },
   ]);
+
+  const [name, setName] = useState('author1');
+
+    const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id);
+      setBlogs(newBlogs);
+    }
+
+  useEffect(() => {
+    console.log('useEffect ran');
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs! - "/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'author1')} title="Author 1's Blogs! - "/>
+      <BlogList blogs={blogs} title="All Blogs! - " handleDelete={handleDelete}/>
+      <button onClick={() => setName("author3")}>Change Name</button>
+      <p>{name}</p>
     </div>
   );
 };
