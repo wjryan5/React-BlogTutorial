321ffa088760a1fdcf6fcfb8e85a4a7b1a8fb196
wjryan5 <43756110+wjryan5@users.noreply.github.com>
2022-11-23 12:22:20 +0000

    17
    local json server

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,30 +1,25 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
-const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "New Website", body: "lorem ipsum...", author: "author1", id: 1 },
-    { title: "Blog post 2!", body: "lorem ipsum...", author: "author2", id: 2 },
-    { title: "Awful website", body: "lorem ipsum...", author: "author1", id: 3, },
-  ]);
 
-  const [name, setName] = useState('author1');
+  const Home = () => {
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id);
-      setBlogs(newBlogs);
-    }
 
   useEffect(() => {
-    console.log('useEffect ran');
-    console.log(name);
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      setBlogs(data);
+    });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs! - " handleDelete={handleDelete}/>
-      <button onClick={() => setName("author3")}>Change Name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="All Blogs! - "/>}
     </div>
   );
 };
