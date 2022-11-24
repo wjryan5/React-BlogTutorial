d7fb77aec6830407931a1b7a70e92a613182e34d
wjryan5 <43756110+wjryan5@users.noreply.github.com>
2022-11-24 16:38:08 +0000

    28 -

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("author1");
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  };
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSumbit}>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="author1">author1</option>
+          <option value="author2">author2</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
